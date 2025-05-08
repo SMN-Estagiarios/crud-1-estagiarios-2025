9ceb306c6ab2efb0987f5015339b8edeeeeaa377
LeomaxFilho <leomax.filho@gmail.com>
2025-05-08 09:20:14 -0300

    feat/delete-dev1: adicionar o funcao delete

--- crud-estagiarios/crud-dev1/Program.cs
+++ crud-estagiarios/crud-dev1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿﻿static void Remover(List<string> lista)
+{
+    bool eValido = true;
+    int idx;
+    
+    if (lista.Count == 0)
+    {
+        Console.WriteLine("A lista esta vazia. Nao ha nada para apagar!");
+        Console.WriteLine("Digite algo para continuar...");
+        Console.ReadKey(true);
+        return;
+    }
+
+    while (true)
+    {
+        Console.WriteLine("Digite o indice da pessoa que voce gostaria de remover!");
+        eValido = int.TryParse(Console.ReadLine(), out idx);    
+        
+        if ((idx >= lista.Count) | (idx < 0) | !eValido)
+        {
+            Console.WriteLine("Indice invalido!\nDigite algo para continuar...");
+            Console.ReadKey();
+            continue;
+        }
+
+        lista.RemoveAt(idx);
+        return;
+    }
+}
