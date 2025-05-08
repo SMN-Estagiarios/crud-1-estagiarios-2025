2e7cea41c53777feaafe22d841117a8bc1496497
IcaroRaphael <icaro2551@gmail.com>
2025-05-08 08:22:03 -0300

    icaro-dev2-create: Método Cadastrar() implementado

--- crud-estagiarios/crud-dev2/Program.cs
+++ crud-estagiarios/crud-dev2/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> itens = new List<string>();
+
+void Cadastrar()
+{
+    Console.Write("Insira um item: ");
+    string item = Console.ReadLine();
+    itens.Add(item);
+}
