267d4b85c091131310104c8040f273f700489b33
isaaccavalcante21 <isaaccavalcantefranco@gmail.com>
2025-05-08 09:38:12 -0300

    feat/read-dev1: cadastro adicionado

--- crud-estagiarios/crud-dev1/Program.cs
+++ crud-estagiarios/crud-dev1/Program.cs
@@ -1,8 +1,24 @@
 ﻿list<string> lista = new list<string>;
 int contador = 1;
 
+void cadastro()
+{
+
+    Console.WriteLine("Digite o nome:");
+    nomeCadastro = Console.ReadLine();
+
+    Console.WriteLine("Digite a idade:");
+    nomeIdade = Console.ReadLine();
+
+    lista[contador] = (nomeCadastro, nomeIdade);
+
+    contador++;
+    
+}
+
 void lista()
 {
+    contador = 1;
 
     foreach (var i in lista)
     {
